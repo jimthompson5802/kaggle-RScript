3e43f527eb8a99c1457a0aa8e8fdf6f04abae90e
Jim Thompson <jimthompson5802@aol.com>
2015-08-09 07:46:27 -0400

    initial logic to get member location

--- src/top3_player_location_extract.R
+++ src/top3_player_location_extract.R
@@ -35,9 +35,23 @@ start_session(root="http://127.0.0.1:4444/wd/hub/",browser="firefox")
 implicit_wait(5000)  # wait for 5 seconds to locate elements on web page
 
 
-getMemberLocation <- function(member) {
+getMemberLocation <- function(onerow) {
     
-    member$location <- NA
+     #retrieve member profile page
+     post.url(url=paste0(PLAYER.URL.PREFIX,onerow$member.url))
+     element_xpath_find('//*[@id="profile-bio"]/h2')
+     
+     #get member page data
+     member.page <- htmlParse(page_source())
+     
+     #find user location
+     user.location <- xmlValue(xpathApply(member.page,'//*[@id="profile2-bio-vitals"]/dd')[[1]])
+     
+     if (nchar(user.location) > 0 ) {
+         onerow$member.location <- geocode(user.location,output = "more")$country
+     } else {
+         onerow$member.location <- "unknown"
+     }
     
     return(member)
 }
@@ -50,25 +64,7 @@ top3.df <- merge(top3.df,medal.df,by=c("competition.name","team.place"))
 # determine member location
 member.location <- sapply(member.url,function(url.frag){
     #             if (!is.na(member.url)) {
-    #                 #retrieve member profile page
-    #                 post.url(url=paste0(PLAYER.URL.PREFIX,url.frag))
-    #                 # Sys.sleep(2)
-    #                 element_xpath_find('//*[@id="profile-bio"]/h2')
-    #                 
-    #                 #get member page data
-    #                 member.page <- htmlParse(page_source())
-    #                 
-    #                 #find user location
-    #                 user.location <- xmlValue(xpathApply(member.page,'//*[@id="profile2-bio-vitals"]/dd')[[1]])
-    #                 
-    #                 if (nchar(user.location) > 0 ) {
-    #                     geocoded.location <- geocode(user.location,output = "more")$country
-    #                 } else {
-    #                     geocoded.location <- "unknown"
-    #                 }
-    #                 
-    #                 page_back()
-    #                 element_xpath_find('//*[@id="leaderboard-table"]/tbody/tr[1]/th[1]')
+
     #             } else {
     #                 geocoded.location <- NA
     #             }
